cc8c4f8757c27650d28c9694beb33f7236777a73
sarahvue <sarahvue0924@gmail.com>
2025-05-08 21:44:52 -0700

    added code

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
 
+  const login = (passwordAttempt) => {
+    attemptCount++;
 
+    if (passwordAttempt === userInfo.password && attemptCount <= 3) {
+      return "Success!";
+    } else if (passwordAttempt !== userInfo.password && attemptCount < 3) {
+      return "Unsuccessful, try again";
+    } else {
+      return "You are blocked!";
+    }
+  };
+
+  return login;
+}
+
+const test = createLoginTracker({ username: "user", password: "password" });
+console.log(test("pasword"));
+console.log(test("pasword"));
+console.log(test("passwordd"));
+console.log(test("password"));
 
 module.exports = {
-  ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
+  ...(typeof createLoginTracker !== "undefined" && { createLoginTracker }),
 };
